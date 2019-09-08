b06038117baa4ca51a45762f890acaf6db0fe471
kevinpollet <pollet.kevin@gmail.com>
2019-09-08 19:13:10 +0400

    Add cmd folder

---
+++ cmd/pocket-remove-duplicates/main.go
@@ -0,0 +1,43 @@
+/**
+ * Copyright © 2019 kevinpollet <pollet.kevin@gmail.com>`
+ *
+ * Use of this source code is governed by an MIT-style license that can be
+ * found in the LICENSE.md file.
+ */
+
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/kevinpollet/pocket-remove-duplicates/pkg/pocket"
+)
+
+func main() {
+	pocketClient := pocket.Client{
+		ConsumerKey: os.Getenv("CONSUMER_KEY"),
+	}
+
+	err := pocketClient.Authorize()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	res, err := pocketClient.Get(&pocket.GetParams{ContentType: "video"})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	d := make(map[string]*pocket.Item, 0)
+
+	for _, item := range res.List {
+		existing := d[item.ResolvedURL]
+		if existing == nil {
+			d[item.ResolvedURL] = &item
+		} else {
+			fmt.Printf("--> Duplicate: %s\n", item.ResolvedTitle)
+		}
+	}
+}
