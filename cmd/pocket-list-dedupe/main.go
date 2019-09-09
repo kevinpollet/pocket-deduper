932a5a842de739e73073549f5cec9923c10b98e7
kevinpollet <pollet.kevin@gmail.com>
2019-09-09 16:57:08 +0400

    Remove unnecessary root.go

--- cmd/pocket-list-dedupe/main.go
+++ cmd/pocket-list-dedupe/main.go
@@ -7,6 +7,59 @@
 
 package main
 
+import (
+	"log"
+
+	"github.com/kevinpollet/pocket-list-dedupe/pocket"
+	"github.com/spf13/cobra"
+)
+
+var (
+	consumerKey string
+	rootCmd     = &cobra.Command{
+		Use:   "pocket-list-dedupe",
+		Short: "Remove duplicate items in your Pocket list",
+		Run: func(cmd *cobra.Command, args []string) {
+			pocketClient := pocket.Client{
+				ConsumerKey: consumerKey,
+			}
+
+			if err := pocketClient.Authorize(); err != nil {
+				log.Fatal(err)
+			}
+
+			res, err := pocketClient.Get(&pocket.GetParams{})
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			temp := make(map[string]*pocket.Item, 0)
+			deleteActions := make([]interface{}, 0)
+
+			for _, item := range res.List {
+				if existingItem := temp[item.ResolvedURL]; existingItem == nil {
+					temp[item.ResolvedURL] = &item
+				} else {
+					deleteActions = append(deleteActions, pocket.DeleteAction{
+						Action: "delete",
+						ItemID: item.ItemID,
+					})
+				}
+			}
+
+			_, err = pocketClient.Modify(deleteActions)
+			if err != nil {
+				log.Fatal(err)
+			}
+		},
+	}
+)
+
 func main() {
-	rootCmd.Execute()
+	rootCmd.Flags().StringVarP(&consumerKey, "consumerKey", "c", "", "Pocket application's Consumer Key")
+	rootCmd.MarkFlagRequired("consumerKey")
+
+	if err := rootCmd.Execute(); err != nil {
+		log.Fatal(err)
+	}
 }
